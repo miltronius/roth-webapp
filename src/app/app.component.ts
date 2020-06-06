ed4fdb686107f352537cfe10873b2136d5e467e9
Miles07 <mail@miltonroth.ch>
2020-06-07 01:42:45 +0200

    add new design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
-  title = 'roth-webapp';
+export class AppComponent implements OnInit, OnDestroy {
+    public title = 'roth-webapp';
+    public counter: number = 0;
+    public isTop: boolean = true;
+
+    constructor(private cdr: ChangeDetectorRef) {
+    }
+
+    ngOnInit(): void {
+    }
+
+    public countUp(event: Event) {
+        console.log(JSON.stringify(event));
+        this.counter++;
+        this.cdr.markForCheck();
+    }
+
+    ngOnDestroy(): void {
+    }
+
+    @HostListener('window:scroll', [])
+    onWindowScroll() {
+        this.isTop = window.scrollY === 0;
+    }
 }
